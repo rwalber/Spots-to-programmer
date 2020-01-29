37967d52bf65a1b38e0741ae636a2ec9e5161dc3
Walber Rocha <walber_jesus@hotmail.com>
2020-01-29 14:47:39 -0300

    adjust thumbnail url

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
 import { withNavigation } from 'react-navigation';
-import { View, Image, StyleSheet, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text, TouchableOpacity, FlatList } from 'react-native';
 
 import api from '../services/api';
 import styles from './SpotList_styles';
@@ -21,13 +21,7 @@ function SpotList({ tech, navigation }) {
     async function handleSubmit(id) {
         navigation.navigate('Book', { id })
     }
-
-    function teste(item) {
-        const tt = item.split(':');
-        // const t = item.split(':');
-        console.log(`'${tt[0]}://192.168.0.106:${tt[2]}'`);
-    }
-    
+ 
     return(
         <View style = { styles.container }>
             <Text style = { styles.title }>Empresas incréveis que utilizam <Text style = {styles.bold}>{tech}</Text></Text>
@@ -39,11 +33,9 @@ function SpotList({ tech, navigation }) {
                 showsHorizontalScrollIndicator = {false}
                 renderItem = {({ item }) => (
                     <View style = { styles.listItem }>
-                        <Image style = { styles.thumbnail } source = {{uri:"https://reactjs.org/logo-og.png"}} />
-                        {/* <Image style = { styles.thumbnail } source = {{ uri: `'${item.thumbnail_url}'` }} /> */}
+                        <Image style = { styles.thumbnail } source = {{ uri: item.thumbnail_url.replace('localhost', '192.168.0.106') }} />
                         <Text style = { styles.company }>{ item.company }</Text>
                         <Text style = { styles.price }> { item.price ? `R$${item.price}/dia` : 'Gratuito' }</Text>
-                        {/* <TouchableOpacity onPress = {() => teste(item.thumbnail_url) } style = { styles.button }> */}
                         <TouchableOpacity onPress = {() => handleSubmit(item._id) } style = { styles.button }>
                             <Text style = { styles.buttonText }>Solicitar reserva</Text>
                         </TouchableOpacity>
